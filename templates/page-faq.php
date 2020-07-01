3247830a24dc4ec8cd6ea299ecff4a8576e3dc7c
Iron318i <iron318i@gmail.com>
2020-07-01 12:57:35 +0300

    Faq page fix

--- templates/page-faq.php
+++ templates/page-faq.php
@@ -1,49 +1,48 @@
 <?php
-
 /**
  * Template Name: FAq
  */
-
 get_header();
-
 ?>
 
 <div class="faq-page">
-	<?php get_template_part('template-parts/template', 'hero'); ?>
-	<section class="white-block">
-		<div class="container">
-			<div class="row">
-				<div class="column col-md-12">
-					<div class="content">
-						<h2 class="faq-title"><?php the_field('main_text_title') ?></h2>
-						<div id="accordion">
-							<?php if( have_rows('faq_accordion') ):
-								while ( have_rows('faq_accordion') ) : the_row();?>
+    <?php get_template_part('template-parts/template', 'hero'); ?>
+    <section class="white-block">
+	<div class="container">
+	    <div class="row">
+		<div class="column col-md-12">
+		    <div class="content">
+			<h2 class="faq-title"><?php the_field('main_text_title') ?></h2>
+			<div id="accordion">
+			    <?php
+			    if (have_rows('faq_accordion')):
+				while (have_rows('faq_accordion')) : the_row();
+				    ?>
 
-									<div class="accordion__item">
-										<label> <?php the_sub_field('questions');?> </label>
-										<div>
-                                        <span>
-                                        <?php the_sub_field('unswers');?>
-                                        </span>
-										</div>
-									</div>
-
-								<?php endwhile;
-							else : ?>
-
-								<h5 class="no-questions">No questions</h5>
-
-							<?php endif;?>
-						</div>
+				    <div class="accordion__item">
+					<label> <?php the_sub_field('questions'); ?> </label>
+					<div>
+	<?php the_sub_field('unswers'); ?>
 					</div>
-				</div>
-			</div>
-		</div>
-	</section>
+				    </div>
 
-	<?php get_template_part('template-parts/form','question'); ?>
+				<?php
+				endwhile;
+			    else :
+				?>
+    			    <h5 class="no-questions">No questions</h5>
+
+<?php endif; ?>
+			</div>
+		    </div>
+		</div>
+	    </div>
+	</div>
+    </section>
+
+<?php get_template_part('template-parts/form', 'question'); ?>
 </div>
 
 
-<?php get_footer();
+<?php
+get_footer();
