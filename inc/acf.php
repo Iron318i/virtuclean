6b18bdf3a722870e43a5deabaddadeb7f131c7a8
Iron318i <iron318i@gmail.com>
2020-07-01 11:09:56 +0300

    Home page fix - 2

--- inc/acf.php
+++ inc/acf.php
@@ -3,25 +3,26 @@
 /**
  * Add Custom fields
  */
-if ( function_exists( 'acf_add_local_field_group' ) ) {
-	require_once 'acf/index.php';
+if (function_exists('acf_add_local_field_group')) {
+    require_once 'acf/index.php';
 }
 
 /**
  * Add ACF PRO OPTION PAGE
  */
-/*if (current_user_can('administrator')) {
-	if(function_exists('acf_add_options_page')) {
-		acf_add_options_page();
-	}
-}*/
+/* if (current_user_can('administrator')) {
+  if(function_exists('acf_add_options_page')) {
+  acf_add_options_page();
+  }
+  } */
 
 /**
  * ACF map API
  */
 function map_acf_init() {
 
-	acf_update_setting('google_api_key', 'xxx');
+    acf_update_setting('google_api_key', 'xxx');
 }
 
 add_action('acf/init', 'map_acf_init');
+
